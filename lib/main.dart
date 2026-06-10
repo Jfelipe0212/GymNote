09d6f67ffe5493daceb19093be0f2331fbd9604f
José Felipe Bandeira . DEV <jfbsilva1202@gmail.com>
2026-06-10 09:43:06 -0300

    Refactor main function and update app title
    Removed Firebase initialization and changed app title from 'FitLog' to 'GymNote'.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -15,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FitLog',
+      title: 'GymNote',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         colorScheme: const ColorScheme.dark(
